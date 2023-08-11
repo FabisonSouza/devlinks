35df1a131ee97fdc4586e011f139b25e55d1a8b9
Fabison Souza <fabisonsouza@hotmail.com>
2023-08-11 17:05:11 -0300

    update js img src

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ function toggleMode() {
 
     if(html.classList.contains('light')) {
 
-        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute("src", './assets/avatar-light.png')
     } else {
-        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute("src", './assets/avatar.png')
     }
-
-
-
 }
